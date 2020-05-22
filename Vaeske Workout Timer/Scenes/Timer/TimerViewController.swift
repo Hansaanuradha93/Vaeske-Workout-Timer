60a34921d77e97e631164c42400ac231ae34f0fc
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-05-22 22:29:13 +0530

    Timer View Controller basic configurations

--- Vaeske Workout Timer/Scenes/Timer/TimerViewController.swift
+++ Vaeske Workout Timer/Scenes/Timer/TimerViewController.swift
@@ -2,6 +2,19 @@ import UIKit
 
 class TimerViewController: UIViewController {
 
+    // MARK: Properties
+    public class var storyboardName: String { return "Timer" }
+    
+    
+    static func create() -> TimerViewController {
+        
+        let storyboard = UIStoryboard(name: storyboardName, bundle: Bundle(for: self))
+        let viewController = storyboard.instantiateViewController(withIdentifier: String(describing: TimerViewController.self)) as? TimerViewController
+        return viewController!
+    }
+    
+    
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
     }
